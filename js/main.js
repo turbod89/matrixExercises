2289de6dcff866eb0ab0af94305c70dbcf33e327
Daniel Torres <dtorres@mequedouno.com>
2018-04-10 17:58:05 +0200

    class changed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 const Matrix = function (n,m) {
+
+    const values = new Float64Array(n*m)
     
-    Object.defineProperties(this, {
+    const matrixFunc = function() {
+        if (arguments.length == 2 && typeof arguments[0] === 'number' && typeof arguments[1] === 'number') {
+            return values[arguments[0]*m + arguments[1]]
+        } else if (arguments.length == 1 && arguments[0].__isMatrix) {
+            return matrixFunc.multiply(arguments[0])
+        }
+    }
+
+    Object.defineProperties(matrixFunc, {
+        __isMatrix: {
+            enumerable: false,
+            configurable: false,
+            get: () => true,
+        },
         rows: {
             enumerable: false,
             configurable: false,
@@ -11,41 +26,48 @@ const Matrix = function (n,m) {
             configurable: false,
             get: () => m
         },
-        value: {
-            enumerable: false,
-            configurable: false,
-            get: function() {
-                return (i,j) => this[m*i + j]
-            }
-        },
         multiply: {
             enumerable: false,
             configurable: false,
             writable: false,
             value: function (B) {
-                const A = this;
+                const A = matrixFunc;
                 let C = new Matrix(A.rows,B.cols);
-
+    
                 let K = Math.min(A.cols, B.rows);
-
+    
                 for (let i = 0; i < A.rows; i++) {
                     for (let j = 0; j < B.cols; j++) {
-                        const index = i*m+j;
+                        const index = i*A.cols+j;
                         C[index] = 0;
                         for (let k = 0; k < K; k++) {
-                            C[index] += A[i*m + k] * B[k*B.cols + j]
+                            C[index] += A(i,k) * B(k,j)
                         }
                     }
                 }
-
+    
                 return C
             }
         },
     });
 
+    return matrix;
+
 };
 
-Matrix.prototype = Object.create(Float64Array.prototype);
+//Matrix.prototype = Object.create(Float64Array.prototype);
+
+Object.defineProperties(Matrix.prototype, {
+
+    value: {
+        enumerable: false,
+        configurable: false,
+        get: function() {
+            return (i,j) => this[this.cols*i + j]
+        }
+    },
+    
+});
 
 window.addEventListener('load', event => {
 
